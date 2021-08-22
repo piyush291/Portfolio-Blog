c949ec21265dda56f7212eb2054f57ffd10df537
piyush291 <56247184+piyush291@users.noreply.github.com>
2021-08-22 17:41:42 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -156,7 +156,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'piyushsethi291@gmail.com'
-EMAIL_HOST_PASSWORD = 'heypiy8782'
+EMAIL_HOST_PASSWORD = '...........'
 
 #-----------------------------------------------------------------------------------------------------------------------
 
